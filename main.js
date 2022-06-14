9e9faef0f2ec811cf230f4669ea63f1e7c7dad3d
unknown <kai84143@gmail.com>
2022-06-15 00:43:21 +0700

    Fix 3

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const app = express();
-const port = 3000;
+const port = 3000 || process.env.PORT;
 
 var charactor = document.getElementById("dino");
 var obsctracle = document.getElementById("obstrucle");
